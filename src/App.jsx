8d0e02c2f0c7419542fc11ea98dd573d461a47f1
Doragon-RH <r216af@icloud.com>
2024-07-21 00:01:34 +0900

    usestateを使用して、属性値の変更を行う処理を追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import { useState } from 'react'
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  function setDogURL() {
-    const [DogUrl] = useState(
-      'https://images.dog.ceo/breeds/mountain-bernese/n02107683_6596.jpg',
-    )
-    return DogUrl
-  }
-
-  // const DogUrl =
-  //   'https://images.dog.ceo/breeds/mountain-bernese/n02107683_6596.jpg'
+  var [DogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/mountain-bernese/n02107683_6596.jpg',
+  )
 
   return (
     <div>
       <header>書籍管理アプリ</header>
       <h1>犬の画像</h1>
-      <img src={setDogURL()} alt="表示できません" />
+      <img src={DogUrl} alt="表示できません" />
+      <button
+        onClick={() =>
+          setDogUrl(
+            'https://images.dog.ceo/breeds/newfoundland/n02111277_120.jpg',
+          )
+        }
+      >
+        更新
+      </button>
     </div>
   )
 }
