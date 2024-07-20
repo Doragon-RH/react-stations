5683f6f61f00f1d1f82cd33449c27e013353842b
Doragon-RH <r216af@icloud.com>
2024-07-20 22:43:53 +0900

    JSX方式で書き換え

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 // DO NOT DELETE
 
 import './App.css'
-
+import { useState } from 'react'
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  function setDogURL() {
+    const [DogUrl] = useState(
+      'https://images.dog.ceo/breeds/mountain-bernese/n02107683_6596.jpg',
+    )
+    return DogUrl
+  }
+
+  // const DogUrl =
+  //   'https://images.dog.ceo/breeds/mountain-bernese/n02107683_6596.jpg'
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>書籍管理アプリ</header>
+      <h1>犬の画像</h1>
+      <img src={setDogURL()} alt="表示できません" />
     </div>
   )
 }
