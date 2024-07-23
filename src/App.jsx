5ba508f78d6fa7028aacf8d7ed36c077766675a7
Doragon-RH <r216af@icloud.com>
2024-07-23 14:35:45 +0900

    APIを取得し、ランダムな犬の画像に変更するようにプログラムを変更

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,34 @@ export const App = () => {
     'https://images.dog.ceo/breeds/mountain-bernese/n02107683_6596.jpg',
   )
 
+  // // XMLHttpRequestオブジェクトの作成
+  // var request = new XMLHttpRequest()
+
+  // // URLを開く
+  // request.open('GET', 'https://dog.ceo/api/breeds/image/random', true)
+  // request.responseType = 'json'
+  // // レスポンスが返ってきた時の処理を記述
+  // request.onload = function () {
+  //   // レスポンスが返ってきた時の処理
+  //   var data = this.response['message']
+  //   console.log(data)
+  // }
+
+  // // リクエストをURLに送信
+  // request.send()
+
+  const getData = function () {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(apiData => setDogUrl(apiData.message))
+      .catch(e => console.error(e.message))
+  }
   return (
     <div>
-      <header>書籍管理アプリ</header>
+      <header>犬アプリ</header>
       <h1>犬の画像</h1>
       <img src={DogUrl} alt="表示できません" />
-      <button
-        onClick={() =>
-          setDogUrl(
-            'https://images.dog.ceo/breeds/newfoundland/n02111277_120.jpg',
-          )
-        }
-      >
-        更新
-      </button>
+      <button onClick={() => getData()}>更新</button>
     </div>
   )
 }
