d187150c1244c192a553cc49759ae6e514b3a4bf
Doragon-RH <r216af@icloud.com>
2024-07-24 19:59:53 +0900

    犬の種類別にランダムに画像を一つ追加する機能を実装

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,30 +1,25 @@
 // @ts-check
-import { useState } from 'react'
 
 export const BreedsSelect = ({ breeds }) => {
-  const [selectedBreed, setSelectedBreed] = useState('')
   // const listItems = breeds.map((number, index) => (
   //   <option value="" key={index}>
   //     {number}
   //   </option>
   // ))　コメントアウトしているものをアクティブにすると上手くstateが更新されない
-  const handleBreedChange = event => {
-    setSelectedBreed(event.target.value)
-  }
+
   return (
     <>
       {/* <select value={selectedBreed} onChange={handleBreedChange}>
         <option value="">選択してください</option>
         {listItems}
       </select> */}
-      <select value={selectedBreed} onChange={handleBreedChange}>
-        <option value="">選択してください</option>
-        {breeds.map((breed, index) => (
-          <option value={breed} key={index}>
-            {breed}
-          </option>
-        ))}
-      </select>
+
+      <option value="">選択してください</option>
+      {breeds.map((breed, index) => (
+        <option value={breed} key={index}>
+          {breed}
+        </option>
+      ))}
     </>
   )
 }
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -3,19 +3,37 @@ import { useState, useEffect } from 'react'
 import { BreedsSelect } from './BreedsSelect'
 export const DogListContainer = () => {
   var [breeds, setBreeds] = useState([])
-
+  const [selectedBreed, setSelectedBreed] = useState('')
+  var [DogUrl, setDogUrl] = useState('')
   useEffect(() => {
     // 処理
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
       .then(apiData => setBreeds(Object.keys(apiData.message)))
+    // .then(console.log(Object.keys(apiData.message)))
     // .catch(e => console.error(e.message))
   }, [])
-
+  const imageUrl = function () {
+    const URL = `https://dog.ceo/api/breed/${selectedBreed}/images/random/`
+    console.log(URL)
+    console.log(selectedBreed)
+    fetch(URL)
+      .then(res => res.json())
+      .then(apiData => setDogUrl(apiData.message))
+    // .catch(e => console.error(e.message))
+  }
+  const handleBreedChange = event => {
+    setSelectedBreed(event.target.value)
+  }
   return (
     <>
       <h2>犬の品種を選択してください</h2>
-      <BreedsSelect breeds={breeds} />
+      <select value={selectedBreed} onChange={handleBreedChange}>
+        <BreedsSelect breeds={breeds} />
+      </select>
+      <h1>犬の画像</h1>
+      <img src={DogUrl} alt="" />
+      <button onClick={() => imageUrl()}>表示</button>
     </>
   )
 }
